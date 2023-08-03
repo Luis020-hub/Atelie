cf2e646022a3d7d04f9d70fa085558e11418b620
Luis020-hub <guhmonteiro10@hotmail.com>
2023-08-02 21:53:11 -0300

    02/08/2023
    - Protótipo inicial da pagina principal usando LayoutBuilder para responsividade com constraints.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 144, 24, 160),
+            title: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(width: 100),
+                Image(
+                  image: AssetImage("lib/images/logo.png"),
+                  fit: BoxFit.cover,
+                  width: 200,
+                  height: 200,
+                ),
+                Text('Horarios',
+                    style: TextStyle(color: Colors.white, fontSize: 22)),
+                Text('Equipe',
+                    style: TextStyle(color: Colors.white, fontSize: 22)),
+                Text('Serviços',
+                    style: TextStyle(color: Colors.white, fontSize: 22)),
+                Text('Como chegar',
+                    style: TextStyle(color: Colors.white, fontSize: 22)),
+                SizedBox(width: 80),
+              ],
+            ),
+          ),
+          body: null,
+        );
+      },
     );
   }
 }
