2ed824d5bacea890ae0b9074b1da08121f87e531
Luis020-hub <guhmonteiro10@hotmail.com>
2023-09-30 21:03:54 -0300

    30/09/2023
    - Removendo a dependência splash_screen.
    - Corrigindo alguns erros de compatibilidade.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:fama/screens/home/HomeScreen.dart';
-import 'package:fama/screens/loading/loading_screen_mobile.dart';
-import 'package:fama/screens/loading/loading_screen_web.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,24 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          return AnimatedSplashScreen(
-            splash: constraints.maxWidth < 800
-                ? const LoadingScreenMobile()
-                : const LoadingScreenWeb(),
-            nextScreen: const HomeScreen(),
-            splashTransition: SplashTransition.scaleTransition,
-            backgroundColor: Colors.purple,
-          );
-        },
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen());
   }
 }
